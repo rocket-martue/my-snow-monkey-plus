7af943ff00a2ddff2cd7fdb8f1b9eae784ebaa1b
Toshiyuki Honda <rocket.martue@gmail.com>
2025-05-27 08:33:56 +0900

    Update _disable-widget-block-editor.php

--- functions/_disable-widget-block-editor.php
+++ functions/_disable-widget-block-editor.php
@@ -7,5 +7,6 @@
 
 // Disables the block editor from managing widgets in the Gutenberg plugin.
 add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
+
 // Disables the block editor from managing widgets.
 add_filter( 'use_widgets_block_editor', '__return_false' );
